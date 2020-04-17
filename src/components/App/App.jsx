e116eecd86bb2aa4beecd5bdee2e6d6760e8144c
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2020-04-17 12:35:43 +0200

    add jsxclassnames

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import classnames from 'classnames'
+import jsxclassnames from 'jsxclassnames'
 
 import User from '../User'
 import image1 from 'assets/images/mockimage.png'
@@ -9,7 +9,7 @@ import styles from './App.module.scss'
 
 const App = () =>
   <div
-    className={classnames(styles.container, styles.text)}
+    className={jsxclassnames(styles.container, styles.text)}
     data-testid='container'
   >
     <img src={image1} />
